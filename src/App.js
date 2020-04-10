4dff906342e71e470262ff1ed523e2a28547cff2
emilerik <emil.eriksson69@gmail.com>
2020-04-10 18:38:00 +0200

    First commit, rough draft of landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'tachyons'
+import greens from "./assets/GoodGreens-logo-v2.png"
+import icons from "./assets/GoodGreens-icons.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header tc">
+          <h1><a href="/">
+              GoodGreens
+          </a></h1>
       </header>
+      <section className="main-container">
+          <section className="intro">
+              <img className="w-40 greens" src={greens} alt="greens"/>
+              <h1 className="intro-text">EDIBLE MICROGREENS IN YOUR HOME.</h1>
+          </section>
+          <section className="icons-section flex flex-column justify-center items-center">
+              <div className="icons">
+                  <img className="" src={icons} alt="icons"/>
+              </div>
+          </section>
+          <section className="contact">
+              <div className="contact-items">
+              <h1 className="intro-text ma3">Interested? Subscribe to our newsletter!</h1>
+              <div className="">
+                  <div className="ui huge input ph3 block">
+                      <input type="text" name="email_address" data-validate="email" placeholder="E-mail"/>
+                  </div>
+                  <button type="submit" value="Subscribe" className="br2 temp pa3 pointer white f3 subscribe-btn">
+                      Sign-up
+                  </button>
+              </div>
+              </div>
+          </section>
+      </section>
     </div>
   );
 }
